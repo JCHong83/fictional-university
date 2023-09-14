bf2172af9ace5f76598a48b654412ad5e0fa6252
JC Hong <jaechol.hong@outlook.com>
2023-09-14 11:09:39 +0200

    PHP first steps

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$names = array('JC', 'John', 'Jane', 'Meowsalot', 'Barksalot');
+
+$count = 0;
+
+while ($count < count($names)) {
+  echo "<li>Hi, my name is $names[$count]</li>";
+  $count++;
+}
