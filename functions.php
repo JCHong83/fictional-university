84356593010f2d00d00b5fa9a69e82b71f75144d
JC Hong <jaechol.hong@outlook.com>
2023-10-03 15:41:35 +0200

    Campus post type with Google maps API

--- functions.php
+++ functions.php
@@ -32,6 +32,7 @@ function pageBanner($args = NULL) {
 
 function university_files()
 {
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDwQQq-TfhGRswcpMDJmyVs8yg_pNPDjUk', NULL, '1.0', true);
   wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
 
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -55,6 +56,10 @@ add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query)
 {
+  if (!is_admin() and is_post_type_archive('campus') and is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
+  
   if (!is_admin() and is_post_type_archive('program') and is_main_query()) {
     $query->set('orderby', 'title');
     $query->set('order', 'ASC');
@@ -78,3 +83,10 @@ function university_adjust_queries($query)
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api) {
+  $api['key'] = 'AIzaSyDwQQq-TfhGRswcpMDJmyVs8yg_pNPDjUk';
+  return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
--- single-program.php
+++ single-program.php
@@ -85,6 +85,20 @@ while (have_posts()) {
       }
     }
 
+    wp_reset_postdata();
+    $relatedCampuses = get_field('related_campuses');
+
+    if ($relatedCampuses) {
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available at These Campuses:</h2>';
+
+      echo '<ul class="min-list link-list">';
+      foreach($relatedCampuses as $campus) {
+        ?> <li><a href="<?php echo the_permalink($campus); ?>"><?php echo get_the_title($campus); ?></a></li> <?php
+      }
+      echo '</ul>';
+    }
+
     ?>
 
   </div>
